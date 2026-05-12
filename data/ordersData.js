2d7078418cdda6cca3151d28d0be04fa524fb7eb
Nikita <pornrarat5441@gmail.com>
2026-05-12 23:09:37 +0700

    add file for choosing waiter for pi

--- data/ordersData.js
+++ data/ordersData.js
@@ -4,33 +4,58 @@ const availableWaiter = waiters.find(w => w.status === "available");
 
 const orders = [
   {
-  //   id: "A03",
-  //   table: 1,
-  //   time: getRealTimeforOrder(),
-  //   status: "prepare",
-  //   menus: [
-  //     {
-  //       name: "Strawberry Milkshake",
-  //       qty: 1
-  //     },
-  //     {
-  //       name: "Pizza Magarita (S)",
-  //       qty: 2
-  //     }
-  //   ],
-  //   waiterName: availableWaiter.name,
-  //   servingStatus: "inprocess",
-  //   servingTime: "20:00",
-  //   deviceId: availableWaiter.deviceId
-  // }
-      id: "A09",
-      table: 9,
+      id: "A20",
+      table: 1,
       time: getRealTimeforOrder(),
       status: "prepare",
       menus: [
         {
-          name: "Macaron",
-          qty: 10
+          name: "Milk Tea",
+          qty: 2
+        },
+        {
+          name: "Chocolate Pancake with Ice Cream",
+          qty: 1
+        }
+      ],
+      waiterName: null,
+      servingStatus: null, //receive data from pi if the waiter click check mark for serving the order **
+      servingTime: null, //serve at the time we click to choose the server
+      deviceId: null
+  },
+    {
+      id: "A21",
+      table: 2,
+      time: getRealTimeforOrder(),
+      status: "prepare",
+      menus: [
+        {
+          name: "Caramel Macchiato",
+          qty: 2
+        },
+        {
+          name: "pizza with cheese",
+          qty: 1
+        }
+      ],
+      waiterName: null,
+      servingStatus: null, //receive data from pi if the waiter click check mark for serving the order **
+      servingTime: null, //serve at the time we click to choose the server
+      deviceId: null
+  },
+    {
+      id: "A22",
+      table: 3,
+      time: getRealTimeforOrder(),
+      status: "prepare",
+      menus: [
+        {
+          name: "Orange Juice with Ice",
+          qty: 2
+        },
+        {
+          name: "Dirty Chocolate Donut (XXL)",
+          qty: 1
         }
       ],
       waiterName: null,
