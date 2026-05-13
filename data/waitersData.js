ee42cf096bab68c9ef557abb28533fa56cf5eb5e
Nikita <pornrarat5441@gmail.com>
2026-05-13 12:51:20 +0700

    add order_id to waiters schema

--- data/waitersData.js
+++ data/waitersData.js
@@ -1,19 +1,20 @@
 const waiters = [
-  {
-  name: "John",
-  status: "available",
-  deviceId: "A01"
-  },
-  {
-  name: "Jane",
-  status: "available",
-  deviceId: "A02"
-  },
+  // {
+  // name: "John",
+  // status: "available",
+  // deviceId: "A01"
+  // },
+  // {
+  // name: "Jane",
+  // status: "available",
+  // deviceId: "A02"
+  // },
     {
-  name: "Andy",
-  status: "available",
-  deviceId: "A02"
-  }
+    name: "Andy",
+    status: "available",
+    deviceId: "A02",
+    order_id: null
+    }
 ];
 
 //if available waiter is already assigned to the order, assign the next available waiter to the order
