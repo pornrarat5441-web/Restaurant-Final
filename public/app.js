a4814357680569a2ff63aecb74bc8e2197f8b81c
Nikita <pornrarat5441@gmail.com>
2026-05-15 13:58:50 +0700

    revert to fixing bug 2

--- public/app.js
+++ public/app.js
@@ -159,7 +159,7 @@ function buildCard(order) {
 function renderOrders() {
   console.log('Rendering orders...');
   const container = document.getElementById('orders-container');
-  const visibleOrders = ordersData;
+  const visibleOrders = ordersData.filter(order => !order.waiterName);
   console.log('Visible orders count:', visibleOrders.length);
 
   container.innerHTML = visibleOrders.map(order => {
